78d679923a74c17910aa0d82b1d0f8096aa4f40f
yougo <yougo@DESKTOP-SFBNH3J>
2024-03-12 01:10:19 +0900

    プロジェクト ファイルを追加します。

---
+++ JumpKing-FreeFlyKingMod/Behaviours/FreeFlyKingBehaviour.cs
@@ -0,0 +1,61 @@
+﻿using JumpKing.API;
+using JumpKing.BodyCompBehaviours;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace JumpKing_FreeFlyKingMod.Behaviours
+{
+    public class FreeFlyKingBehaviour : IBodyCompBehaviour
+    {
+        private bool isActive = false;
+        private bool freeFlyingToggleCooldown = false;
+
+        private const Keys toggleKey = Keys.H;
+
+        public bool ExecuteBehaviour(BehaviourContext behaviourContext)
+        {
+            KeyboardState state = Keyboard.GetState();
+            
+            if (state.IsKeyDown(toggleKey) && !freeFlyingToggleCooldown)
+            {
+                isActive = !isActive;
+                freeFlyingToggleCooldown = true;
+            }
+            else if (state.IsKeyUp(toggleKey) && freeFlyingToggleCooldown)
+            {
+                freeFlyingToggleCooldown = false;
+            }
+
+            if (isActive)
+            {
+                Vector2 velocity = behaviourContext.BodyComp.Velocity;
+                float curX = 0;
+                float curY = 0;
+
+                if (state.IsKeyDown(Keys.W))
+                {
+                    curY -= 3;
+                }
+                if (state.IsKeyDown(Keys.A))
+                {
+                    curX -= 3;
+                }
+                if (state.IsKeyDown(Keys.D))
+                {
+                    curX += 3;
+                }
+                if (state.IsKeyDown(Keys.S))
+                {
+                    curY += 3;
+                }
+
+                velocity.X = curX;
+                velocity.Y = curY;
+
+                behaviourContext.BodyComp.Velocity = velocity;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ JumpKing-FreeFlyKingMod/ModEntry.cs
@@ -0,0 +1,26 @@
+﻿using EntityComponent;
+using JumpKing.Mods;
+using JumpKing.Player;
+
+namespace JumpKing_FreeFlyKingMod
+{ 
+    [JumpKingMod("YutaGoto.FreeFlyKingMod")]
+    public class ModEntry
+    {
+        [OnLevelStart]
+        public static void OnLevelStart()
+        {
+            PlayerEntity player = EntityManager.instance.Find<PlayerEntity>();
+            if (player != null)
+            {
+                player.m_body.RegisterBehaviour(new Behaviours.FreeFlyKingBehaviour());
+            }
+        }
+
+        [OnLevelEnd]
+        public static void OnLevelEnd()
+        {
+            
+        }
+    }
+}
